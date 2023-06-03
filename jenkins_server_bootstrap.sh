eb346211dd2af84ce013ba0b128bb4ad16a6b996
Afajobi <105067655+Acerock1@users.noreply.github.com>
2023-06-03 01:57:18 +0000

    create jenkins server bootstrap

---
+++ jenkins_server_bootstrap.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+sudo apt-get update
+sudo apt-get zip unzip
+
+
+#install git
+
+sudo apt-get install git
+
+#install jenkins
+
+sudo apt update
+sudo apt install -y openjdk-11-jre 
+
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+
+#install git
+
+sudo apt-get install git -y
+
+#install terraform
+
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform -y
